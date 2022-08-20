4baee7b2811e625ab7cdea7835f646c642aa1be4
Harun Akgul <46harunakgul@gmail.com>
2022-08-20 19:37:21 +0300

    CSS file edited.

--- index.js
+++ index.js
@@ -8,29 +8,29 @@ function calculate(){
 
 
     if(calculate >= 90 && calculate <= 100){
-        document.getElementById("result").innerHTML = "Average :" + calculate + "=> AA";
+        document.getElementById("result").innerHTML = "Average :" + calculate + " => AA";
     }
     else if (calculate >= 75 && calculate < 90) {
-        document.getElementById("result").innerHTML = "Average :" + calculate + "=> BA";
+        document.getElementById("result").innerHTML = "Average :" + calculate + " => BA";
     }
     else if (calculate >= 65 && calculate < 75) {
         console.log(calculate);
-        document.getElementById("result").innerHTML = "Average :" + calculate + "=> BB";
+        document.getElementById("result").innerHTML = "Average :" + calculate + " => BB";
     }
     else if (calculate >= 55 && calculate < 65) {
-        document.getElementById("result").innerHTML = "Average :" + calculate + "=> CB";
+        document.getElementById("result").innerHTML = "Average :" + calculate + " => CB";
     }
     else if (calculate >= 50 && calculate < 55) {
-        document.getElementById("result").innerHTML = "Average :" + calculate + "=> CC";
+        document.getElementById("result").innerHTML = "Average :" + calculate + " => CC";
     }
     else if (calculate >= 45 && calculate < 50) {
-        document.getElementById("result").innerHTML = "Average :" + calculate + "=> DC";
+        document.getElementById("result").innerHTML = "Average :" + calculate + " => DC";
     }
     else if (calculate >= 40 && calculate < 45) {
-        document.getElementById("result").innerHTML = "Average :" + calculate + "=> DD";
+        document.getElementById("result").innerHTML = "Average :" + calculate + " => DD";
     }
     else{
-        document.getElementById("result").innerHTML = "Average :" + calculate + "=> FF";
+        document.getElementById("result").innerHTML = "Average :" + calculate + " => FF";
     }  
     
 
